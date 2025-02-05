53bff37aa51c6af4f01d17fa197e8e98411215c1
Raju <rajukumar1231221@gmail.com>
2025-02-06 00:40:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function call(){
+    let input = document.getElementById('#input');
+
+    switch (input) {
+        case input:
+            input+1
+            break;
+    
+        default:
+            break;
+    }
+}
---
+++ lms/README.md
@@ -0,0 +1,20 @@
+react
+router dom
+redux
+axios
+react icons
+react hot toast
+ tailwind css
+  4
+
+
+  backend
+  express
+  mongoose
+  jwt
+  bcypt
+  ejs
+  morgan
+  multer
+  nodemailer
+  cors
